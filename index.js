ff96323046f2fa77996f447956700eeeb26ea855
SUJOY GIRI <sujoy.giri@infosys.com>
2023-08-25 11:15:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let box = document.getElementById('box')
+box.style.height = '100px'
+box.style.width = '100px'
+
+
+function display(){
+    console.log(document.body.clientHeight);
+    console.log(document.body.scrollHeight);
+    console.log(document.body.getBoundingClientRect());
+}
+
+display()
